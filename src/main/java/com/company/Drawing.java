b965be0b7e5d2c14d1fcc813d90df785d17986f4
M0G1 <myslim2000@ya.ru>
2022-06-13 12:19:13 +0400

    Added working black/white visualization(without coloring)

--- src/main/java/com/company/Drawing.java
+++ src/main/java/com/company/Drawing.java
@@ -1,5 +1,6 @@
 package com.company;
 
+import com.company.model.Rect;
 import com.company.model.TriShape;
 import com.company.util.color.Color;
 
@@ -9,20 +10,17 @@ import java.util.List;
 import static com.company.util.color.Color.colorize;
 
 public class Drawing {
-    public static String fillingStr = "--"; // string what will be placed into triangle. Must have size = 2
 
-    private enum aShapeState {
-        empty, // предыдущий треугольник - пустой
-        longEmpty, // несколько предыдущих треугольников - пустые
-        odd, // предыдущий треугольный элемент закрашен и нечетный
-        even // предыдущий треугольный элемент закрашен и четный
-    }
+    public static String evenSubTri = "\\--/";
+    public static String evenSubTri2 = "\\/";
+    public static String oddSubTri = "/\\";
+    public static String oddSubTri2 = "/--\\";
 
-    public static String drawWithFill(List<TriShape> triShapes, List<String> textColor, List<String> backgroundColor){
+    public static String drawWithFill(List<TriShape> triShapes, List<String> textColor, List<String> backgroundColor) {
         return "";
     }
 
-    @Deprecated
+
     public static String draw(TriShape triShape, String textColor, String backgroundColor) {
         int[][] tri = triShape.getTri();
         StringBuilder ans = new StringBuilder(
@@ -30,75 +28,55 @@ public class Drawing {
                         Arrays.stream(tri).mapToInt(value -> value.length).sum() *
                                 (textColor.length() + backgroundColor.length() + Color.RESET.length()) // it may be wrong
         );
-        StringBuilder first, second;
-        for (int i = 0; i < tri.length; ++i) {
-            first = new StringBuilder((i & 1) == 0 ? "" : " "); // выставляем первый символ для отступа
-            second = new StringBuilder((i & 1) == 0 ? " " : "");
-            int j = 0, ind = 0; // индекс треугольного элемента в матрице и индекс в массиве треугльных элементов фигруы
-            while (ind < tri[i].length) {
-                int nextTriInd = tri[i][ind]; // индекс следующего треугольного элемента для закраски
-                if (nextTriInd > j) // добавляем пустые треугольники
-                    addEmptyTriangles(i, j, nextTriInd, first, second);
-                j = nextTriInd;
-                if (((i + j) & 1) == 0)
-                    addEvenFilledTriangle(first, second, textColor, backgroundColor);
-                else
-                    addOddFilledTriangle(first, second, textColor, backgroundColor);
-                ++ind;
+        int symPerSubRow = fillStringBuilder(ans, triShape.getOutscribedRect());
+        for (int[] ints : tri) {
+            int i = ints[0], j = ints[1];
+            int fromSub1, fromSub2, toSub1, toSub2;
+            int halfJ = j / 2;
+            if ((i & 1) == 0) { // odd
+                if ((j & 1) == 0) {
+                    fromSub1 = halfJ * 4;
+                    toSub1 = fromSub1 + 3;
+                    fromSub2 = 1 + 4 * halfJ;
+                    toSub2 = fromSub2 + 1;
+                } else {
+                    fromSub1 = 3 + halfJ * 4;
+                    toSub1 = fromSub1 + 1;
+                    fromSub2 = 2 + 4 * halfJ;
+                    toSub2 = fromSub2 + 3;
+                }
+            } else {
+                if ((j & 1) == 0) {
+                    fromSub1 = 1 + halfJ * 4;
+                    toSub1 = fromSub1 + 1;
+                    fromSub2 = 4 * halfJ;
+                    toSub2 = fromSub2 + 3;
+                } else {
+                    fromSub1 = 2 + halfJ * 4;
+                    toSub1 = fromSub1 + 3;
+                    fromSub2 = 3 + 4 * halfJ;
+                    toSub2 = fromSub2 + 1;
+                }
             }
-            first.append("\n");
-            second.append("\n");
-            ans.append(first).append(second);
+            fromSub1 += 2 * i * symPerSubRow;
+            fromSub2 += (2 * i + 1) * symPerSubRow;
+            toSub1 += (2 * i * symPerSubRow + 1);
+            toSub2 += ((2 * i + 1) * symPerSubRow + 1);
+            if (((i + j) & 1) == 0) {
+                ans.replace(fromSub1, toSub1, evenSubTri);
+                ans.replace(fromSub2, toSub2, evenSubTri2);
+            } else {
+                ans.replace(fromSub1, toSub1, oddSubTri);
+                ans.replace(fromSub2, toSub2, oddSubTri2);
+            }
         }
         return ans.toString();
     }
 
-    @Deprecated
-    private static void addOddFilledTriangle(StringBuilder first, StringBuilder second, String textColor, String backgroundColor) {
-        if (!(first.lastIndexOf("/") == first.length() - 1
-                && second.lastIndexOf("/") == second.length() - 1)) {
-            first.append(colorize(textColor, "/"));
-            second.append(colorize(textColor, "/"));
-        }
-        second.append(colorize(textColor + backgroundColor, fillingStr));
-        first.append(colorize(textColor, "\\"));
-        second.append(colorize(textColor, "\\"));
+    private static int fillStringBuilder(StringBuilder builder, Rect rect) {
+        int symPerSubRow = rect.getWeight() * 4 + 2;
+        String subRow = " ".repeat(symPerSubRow) + "\n";
+        builder.append(subRow.repeat(Math.max(0, rect.getHeight() * 2)));
+        return ++symPerSubRow;
     }
-
-    @Deprecated
-    private static void addEvenFilledTriangle(StringBuilder first, StringBuilder second, String textColor, String backgroundColor) {
-        if (!(first.lastIndexOf("\\") == first.length() - 1
-                && second.lastIndexOf("\\") == second.length() - 1)) {
-            first.append(colorize(textColor, "\\"));
-            second.append(colorize(textColor, "\\"));
-        }
-        first.append(colorize(textColor + backgroundColor, fillingStr));
-        first.append(colorize(textColor, "/"));
-        second.append(colorize(textColor, "/"));
-    }
-
-    @Deprecated
-    private static void addEmptyTriangles(int i, int j, int nextTriInd, StringBuilder first, StringBuilder second) {
-        nextTriInd -= 1; // специальная обработка для последнего треуготольного элемента
-        int diff = nextTriInd - j;
-        if (diff > 1) {
-            int parallelogramCnt = diff / 2;
-            String parallelogramEmpty = " ".repeat(parallelogramCnt * 5);
-            first.append(parallelogramEmpty);
-            second.append(parallelogramEmpty);
-            if (parallelogramCnt * 2 < diff) {
-                int lastFullElInd = diff - 1; // parallelogramCnt * 2 + 1 must be same ??
-                boolean isEven = ((lastFullElInd + i) & 1) == 0;
-                first.append(isEven ? "   " : " ");
-                second.append(isEven ? " " : "   ");
-            }
-        }else {
-            first.append(" ");
-            second.append(" ");
-        }
-        boolean isEven = ((nextTriInd + i) & 1) == 0;
-        first.append(isEven ? "  " : "");
-        second.append(isEven ? "" : "  ");
-    }
-
 }
